c6efc3c90fa730dd7e75c32f3e550522a2ed3d11
shirachin <shirachin1015@gmail.com>
2023-06-15 20:53:18 +0900

    test

--- ATBB_DataPicker.user.js
+++ ATBB_DataPicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ATBB_DataPicker
 // @namespace    https://github.com/shirachin/ATBB_DataPicker
-// @version      1.2.0
+// @version      1.2.1
 // @description  Reads necessary information from ATBB and outputs it in json format.
 // @author       shirachin
 // @match        https://atbb.athome.co.jp/front-web/mainservlet/*
